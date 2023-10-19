2b3c20ddc6b175bccf53186f80ad8c4975e9f33e
sarahellis <sarahellislondon@gmail.com>
2023-10-19 16:56:10 +0100

    updates to session form and app.js route added to /sessions

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import Layout from '../layout/Layout'
+import SessionForm from '../session/SessionForm';
 import {
   useNavigate,
   Routes,
@@ -11,6 +12,7 @@ const App = () => {
     return (
         <Routes>
           <Route path='/'  element={<Layout navigate={ useNavigate() }/>}/>
+          <Route path='/sessions'  element={<SessionForm navigate={ useNavigate() }/>}/>
         </Routes>
     );
 }
--- frontend/src/components/session/SessionForm.js
+++ frontend/src/components/session/SessionForm.js
@@ -1,59 +1,59 @@
-
 import React, { useState } from "react";
+import "./sessionform.css";
 
 const SessionForm = ({ token, setSessions }) => {
     const [date, setDate] = useState("");
     const [location, setLocation] = useState("");
     const [chosenBook, setChosenBook] = useState("")
-}
 
 // setSessions still needs defining (Session.js or Feed.js?)
 
-const handleSubmitSession = async (event) => {
-    event.preventDefault(); 
-    const  formData = new FormData(); 
-    formData.append("date", date);
-    formData.append("location", location);
-    formData.append("chosenBook", chosenBook);
+    const handleSubmitSession = async (event) => {
+        event.preventDefault(); 
+        const  formData = new FormData(); 
+        formData.append("date", date);
+        formData.append("location", location);
+        formData.append("chosenBook", chosenBook);
 
-    if (token) {
-        fetch("/sessions", {
-           method: "POST",
-           headers: {
-            Authorization: `Bearer ${token}`,
-           },
-           body: formData,  
-        })
-        .then((response) => {
-            if (response.status === 201) {
-                console.log("Session successfully added"); 
-                return response.json();      
-            } else {
-                console.log("Session not successfully added");
-            }
-        })
-        .then((data) =>{
-            // update sessions array with new post
-            /* prevSessions is a parameter for the anonymous function. It represents 
-            the current state of sessions at the time the function is executed. */
-            setSessions((prevSessions) => [data.session, ...prevSessions]);
-            setDate("");
-            setLocation("");
-            setChosenBook("");
-        });
-    } else {
-        console.log("No token");
-    }
-};
+        if (token) {
+            fetch("/sessions", {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            body: formData,  
+            })
+            .then((response) => {
+                if (response.status === 201) {
+                    console.log("Session successfully added"); 
+                    return response.json();      
+                } else {
+                    console.log("Session not successfully added");
+                }
+            })
+            .then((data) =>{
+                // update sessions array with new post
+                /* prevSessions is a parameter for the anonymous function. It represents 
+                the current state of sessions at the time the function is executed. */
+                setSessions((prevSessions) => [data.session, ...prevSessions]);
+                setDate("");
+                setLocation("");
+                setChosenBook("");
+            });
+        } else {
+            console.log("No token");
+        }
+    };
 
 // This return session is what gets rendered on the feed
-return (
-    <form
-      id="session-form"
-      /* Finish form! With Figma and Ellie? */
-      >
-        
-    </form>
-)
+    return (
+        <form className="session-form-box">
+        <h1 id="test_title">Sessions</h1> 
+        <textarea className="new-session-form">
+            
+        </textarea>
+        </form>
+    )
 
+}
 export default SessionForm;
