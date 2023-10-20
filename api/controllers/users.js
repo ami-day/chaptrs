639bcd9d1c798e1076ed8a01601f10384ba07f06
elliepriestley <ellie.priestley@gmail.com>
2023-10-20 11:54:01 +0100

    Completed MVP front-end validation for signup page, as well as backend check for existing username/email address

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,25 +1,35 @@
 const User = require("../models/user");
 
 const UsersController = {
-  Create: (req, res) => {
-    const user = new User(req.body);
-    console.log(user);
-    user.save((err) => {
-      if (err) {
-        res.status(400).json({message: 'Bad request'})
-      } else {
-        res.status(201).json({ message: 'OK' });
-      }})},
-  Index: (req, res) => {
-      User.find((err, users) => {
-        if (err) {
-          throw err;
+  Create: async (req, res) => {
+    try {
+      const existingUser = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+      if (existingUser) {
+        if (existingUser.username === req.body.username) {
+          res.status(409).json({ message: 'User already exists' });
+        } else if (existingUser.email === req.body.email) {
+          res.status(409).json({ message: 'User already exists' });
         }
-        const token = TokenGenerator.jsonwebtoken(req.user_id)
-        console.log("Generated Token:", token);
-        res.status(200).json({ users: users, token: token });
-      });
+      } else {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).json({ message: 'OK' });
+      }
+    } catch (err) {
+      res.status(500).json({ message: 'Internal Server Error' });
     }
-  };
+  },
+  Index: async (req, res) => {
+    try {
+      const users = await User.find();
+      // Assuming req.user_id contains the user's ID
+      const token = TokenGenerator.jsonwebtoken(req.user_id);
+      console.log("Generated Token:", token);
+      res.status(200).json({ users: users, token: token });
+    } catch (err) {
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  },
+};
 
 module.exports = UsersController;
