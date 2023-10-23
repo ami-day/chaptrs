6a08e115711663f60dcb7373cfa3a5d6ff3b14dc
sarahellis <sarahellislondon@gmail.com>
2023-10-23 20:13:13 +0100

    Use BookLabel component inside UpcomingEvents and display book title in BookLabel component

--- frontend/src/components/upcomingevents/BookLabel.js
+++ frontend/src/components/upcomingevents/BookLabel.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./booklabel.css";
 // import "../upcomingevents.js";
 
 const BookLabel = ({ sessions }) => {
+  const [token, _] = useState(window.localStorage.getItem("token"));
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    console.log("Checking books");
+    if (token) {
+      fetch("/books", {
+        method: "get",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => response.json())
+        .then(async (data) => {
+          window.localStorage.getItem("token", data.token);
+          setBooks(data.books);
+        });
+    }
+  }, []);
+
+  function bookIdToTitle(book_id) {
+    const foundBook = books.find((book) => book._id === book_id);
+    return foundBook ? foundBook.title : undefined;
+  }
+
   return (
     <div className="label">
       <div className="upcoming-bookclub-book-information">
         {sessions ? (
           sessions.map((session) => (
-            <div key={session._id}>
-              <h3>Title: {session.book}</h3>
+            <div key={session.chosen_book}>
+              <h3>Title: {bookIdToTitle(session.chosen_book)}</h3>
             </div>
           ))
         ) : (
