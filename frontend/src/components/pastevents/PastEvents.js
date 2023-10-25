93dbd1493df472529829bc941fc5be676c25de66
Ami Day <amiday@Amis-MacBook-Pro-2.local>
2023-10-25 14:11:02 +0100

    Added past events logic

--- frontend/src/components/pastevents/PastEvents.js
+++ frontend/src/components/pastevents/PastEvents.js
@@ -1,72 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 import "./pastevents.css";
 import BookLabel from "../upcomingevents/BookLabel";
 import SessionLabel from "../upcomingevents/SessionLabel";
+import CoverLabel from "../upcomingevents/CoverLabel";
+import "../session/SessionForm";
+import sortBy from "lodash/sortBy";
 const book = require('../upcomingevents/book.jpg');
 
-const PastEvents = () => {
-  return (
-    <div className="past-events">
-      <div className="section-title">
-        <div className="content">
-          <div className="text-wrapper">Past Events</div>
-        </div>
-      </div>
-      <div className="div">
-        <div className="row">
-          <div className="past-event-block">
-        <div className="book-wrapper">
-          <img className="book" alt="Book" src={book} />
-          <div className="box">
-            <BookLabel></BookLabel>
-          </div>
-          <div className="box">
-          <SessionLabel></SessionLabel>
-          </div>
-          </div>
-          </div>
+  const PastEvents = () => {
+    const [user, setUser] = useState("");
+    const [pastSessions, setPastSessions] = useState([]);
+    const [token, setToken] = useState(window.localStorage.getItem("token"));
+  
+    function getPastSessions(sessions) {
+      let past = [];
+      let today = new Date();
+      sessions.forEach((session) => {
+        if (new Date(session.date) >= today) {
+          past.push(session);
+        }
+      });
+      return sortBy(past, "date");
+    }
+  
+    useEffect(() => {
+      console.log("Checking sessions");
+      if (token) {
+        fetch("/sessions", {
+          method: "get",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+          .then((response) => response.json())
+          .then(async (data) => {
+            console.log("session data", data.sessions);
+            window.localStorage.getItem("token", data.token);
+            let pastSessions = getPastSessions(data.sessions);
+            setPastSessions(pastSessions);
+          });
+      }
+    }, []);
 
-          <div className="past-event-block">
-        <div className="book-wrapper">
-          <img className="book" alt="Book" src={book} />
-          <div className="box">
-            <BookLabel></BookLabel>
-          </div>
-          <div className="box">
-          <SessionLabel></SessionLabel>
-          </div>
-          </div>
-          </div>
-        </div>
-        <div className="row-2">
-        <div className="past-event-block">
-        <div className="book-wrapper">
-          <img className="book" alt="Book" src={book} />
-          <div className="box">
-            <BookLabel></BookLabel>
-          </div>
-          <div className="box">
-          <SessionLabel></SessionLabel>
-          </div>
-          </div>
-          </div>
-            <div className="indv-row">
-          <div className="past-event-block">
-        <div className="book-wrapper">
-          <img className="book" alt="Book" src={book} />
-          <div className="box">
-            <BookLabel></BookLabel>
-          </div>
-          <div className="box">
-          <SessionLabel></SessionLabel>
-          </div>
-          </div>
-           </div>
-          </div>
-        </div>
-      </div>
-      <div className="content-2" />
+  return (
+<div className="past-events">
+  <div className="section-title">
+    <div className="content">
+      <div className="text-wrapper">Past Events</div>
     </div>
+  </div>
+  {pastSessions ? (
+    <div className="past-event-block">
+      {pastSessions.map((session) => (
+        <div className="book-wrapper">
+          <CoverLabel session={session}></CoverLabel>
+          <div className="box">
+            <BookLabel session={session}></BookLabel>
+          </div>
+          <div className="box">
+            <SessionLabel session={session} />
+          </div>
+        </div>
+      ))}
+    </div>
+  ) : (
+    <div className="past-event-block"></div>
+  )}
+</div>
   );
 };
 
